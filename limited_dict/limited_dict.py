b48c9ddf094052c84c8e85939c470b2f16ece4d8
Rajiv Patel <rajivpatel36@gmail.com>
2015-11-22 19:28:14 +0000

    Added ability to specify whether the mapping is cached (in an internal dict). If it is cached, performance is improved at the expense of more memory.

--- limited_dict/limited_dict.py
+++ limited_dict/limited_dict.py
@@ -3,10 +3,10 @@ from bisect import bisect
 
 
 class LimitedDict(MutableMapping):
-    def __init__(self, max_length):
+    def __init__(self, max_length, cache_mapping=True):
         self._keys = list()
         self._values = list()
-        self._internal_mapping = dict()
+        self._internal_mapping = dict() if cache_mapping else None
         self._max_length = max_length
         self._at_max_length = False
 
@@ -14,10 +14,12 @@ class LimitedDict(MutableMapping):
         index = bisect(self._values, value)
         self._values.insert(index, value)
         self._keys.insert(index, key)
-        self._internal_mapping[key] = value
+        if self._internal_mapping is not None:
+            self._internal_mapping[key] = value
 
         if self._at_max_length:
-            del self._internal_mapping[self._keys[self._max_length]]
+            if self._internal_mapping is not None:
+                del self._internal_mapping[self._keys[self._max_length]]
             del self._keys[self._max_length]
             del self._values[self._max_length]
         else:
@@ -25,7 +27,11 @@ class LimitedDict(MutableMapping):
                 self._at_max_length = True
 
     def __getitem__(self, key):
-        return self._internal_mapping[key]
+        if self._internal_mapping is not None:
+            return self._internal_mapping[key]
+        else:
+            index = bisect(self._keys, key) - 1
+            return self._values[index]
 
     def __delitem__(self, key):
         index = bisect(self._keys, key) - 1
@@ -35,7 +41,7 @@ class LimitedDict(MutableMapping):
         self._at_max_length = False
 
     def __iter__(self):
-        return self._internal_mapping.__iter__()
+        return self._keys.__iter__()
 
     def __len__(self):
         return len(self._keys)
