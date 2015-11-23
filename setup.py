99f80076551d058790c8c5841e3913fc8bd74e60
Rajiv Patel <rajiv@ometria.com>
2015-11-23 14:02:19 +0000

    Added setup files required for PyPI.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'limited-dict',
+  packages = ['limited-dict'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A dictionary-like object which only stores the highest or lowest N values',
+  author = 'Rajiv Patel',
+  author_email = 'rajivpatel36@gmail.com',
+  url = 'https://github.com/rajivpatel36/limited-dict', # use the URL to the github repo
+  download_url = 'https://github.com/rajivpatel36/limited-dict/tarball/0.1', # I'll explain this in a second
+  keywords = ['dict', 'limited', 'sorted', 'sorteddict'], # arbitrary keywords
+  classifiers = [],
+)
