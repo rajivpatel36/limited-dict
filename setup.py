77b73d0bf0296f4517c62aff9bfc3d3348436206
Rajiv Patel <rajiv@ometria.com>
2015-12-07 15:25:45 +0000

    Updated to 0.2.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'limited_dict',
-  packages = ['limited_dict'], # this must be the same as the name above
-  version = '0.1',
+  packages = ['limited_dict'],
+  version = '0.2',
   description = 'A dictionary-like object which only stores the highest or lowest N values',
   author = 'Rajiv Patel',
   author_email = 'rajivpatel36@gmail.com',
-  url = 'https://github.com/rajivpatel36/limited-dict', # use the URL to the github repo
-  download_url = 'https://github.com/rajivpatel36/limited-dict/tarball/0.1', # I'll explain this in a second
-  keywords = ['dict', 'limited', 'sorted', 'sorteddict'], # arbitrary keywords
+  url = 'https://github.com/rajivpatel36/limited-dict',
+  download_url = 'https://github.com/rajivpatel36/limited-dict/tarball/0.2',
+  keywords = ['dict', 'limited', 'sorted', 'sorteddict'],
   classifiers = [],
 )
