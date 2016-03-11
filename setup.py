73777c4cc4c31b11a252a370ab67018e39c70530
Rajiv Patel <rajiv@ometria.com>
2016-03-11 17:48:53 +0000

    Updated to 0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'limited_dict',
   packages = ['limited_dict'],
-  version = '0.2',
+  version = '0.3',
   description = 'A dictionary-like object which only stores the highest or lowest N values',
   author = 'Rajiv Patel',
   author_email = 'rajivpatel36@gmail.com',
   url = 'https://github.com/rajivpatel36/limited-dict',
-  download_url = 'https://github.com/rajivpatel36/limited-dict/tarball/0.2',
+  download_url = 'https://github.com/rajivpatel36/limited-dict/tarball/0.3',
   keywords = ['dict', 'limited', 'sorted', 'sorteddict'],
   classifiers = [],
 )
