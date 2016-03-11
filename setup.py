2666664a0dfa57bd751fc313741dc1d52d1b18ba
Rajiv Patel <rajiv@ometria.com>
2016-03-11 18:01:59 +0000

    Updated to 0.4 because I seemingly screwed up submission to PyPI

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'limited_dict',
   packages = ['limited_dict'],
-  version = '0.3',
+  version = '0.4',
   description = 'A dictionary-like object which only stores the highest or lowest N values',
   author = 'Rajiv Patel',
   author_email = 'rajivpatel36@gmail.com',
   url = 'https://github.com/rajivpatel36/limited-dict',
-  download_url = 'https://github.com/rajivpatel36/limited-dict/tarball/0.3',
+  download_url = 'https://github.com/rajivpatel36/limited-dict/tarball/0.4',
   keywords = ['dict', 'limited', 'sorted', 'sorteddict'],
   classifiers = [],
 )
